9618ec144956811b5c339ffcdb1c9c390d3f607f
Sandra Gimenez <sandragimenez@outlook.es>
2021-01-18 13:01:56 +0100

    leer fichero texto contar caracteres  lineas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-console.log('hola mundo')
-console.log('Sandra')
+//console.log('hola mundo')
+//console.log('Sandra')
+// Muestra el número total de lineas, y el número de palabras por linea
+
+const readline = require('readline');
+const fs = require('fs')
+
+const file = process.argv[2]
+let lines = 0
+
+console.log(`Fichero selecionado: ${file}\n`)
+
+// Emite el evento 'line' cuando lee una linea
+const rl = readline.createInterface({
+    input: fs.createReadStream(file),
+    crlfDelay: Infinity
+})
+
+rl.on('line', (line) => {
+    ++lines
+    console.log(`Número de caracteres por linea: ${line.length}`)
+})
+
+rl.on('close', () => {
+    console.log(`\nNúmero total de lineas: ${lines}`)
+})
