43e5cf5b7875f6baab51c73bf4673fbf22cbec12
Sandra Gimenez <sandragimenez@outlook.es>
2021-01-19 12:42:43 +0100

    nodemon pruebas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-process.on('unhandledRejection', (err, p) => {
-    console.log(`Custom unhandleRejection ${err}`)
-})
-
-process.on('uncaughtException', (err) => {
-    console.log(`Custom uncaughtException ${err}`)
-})
-
-//Promise(resolve => JSON.pare({ color: 'blue' }))
-//test()
-throw 'mi error'
+console.log('hola nodemon')
+console.log('hola actualizado')
